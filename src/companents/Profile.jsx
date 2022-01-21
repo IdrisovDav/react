5bb309d10fe8e0e5d0b47ab9595da93d7e606847
IdrisovDav <idrisovdavlayt@gmail.com>
2022-01-21 17:17:13 +0600

    first commit

---
+++ src/companents/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function Profile(){
+    return(
+      <div className='content'>
+      <div>
+        <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" alt="" />
+      </div>
+      <div>
+        ava
+      </div>
+      <div>
+        my post
+      </div>
+      <div>
+        new post
+      </div>
+      <div>
+        <div>
+          post 1
+        </div>
+        <div>
+          post 2
+        </div>
+      </div>
+    </div>
+    )
+}
+
+export default Profile
